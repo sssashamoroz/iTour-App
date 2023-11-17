0dd18474f47723d5871fb0dd5871ba0467ba64ae
Sasha Morozov <aa.morozov@outlook.com>
2023-11-17 14:56:22 +0100

    Initial Commit

---
+++ iTour-App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iTour-App
+//
+//  Created by Aleksandr Morozov on 17/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iTour-App/iTour_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTour_AppApp.swift
+//  iTour-App
+//
+//  Created by Aleksandr Morozov on 17/11/23.
+//
+
+import SwiftUI
+
+@main
+struct iTour_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
